142bc370dcb30a42f6177fdbeb51975b4d4f97d9
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-28 15:44:58 +0100

    get form value on click

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const button_get=document.getElementById("button-genera");
+
+button_get.addEventListener('click', function(){
+    let input_tag=document.getElementById('km').value;
+    console.log(input_tag)
+    let input_sex=document.getElementById('sex').value;
+    console.log(input_sex)
+})
+
+if(input_sex = 'Maggiorenne'){
+    
+}
+
