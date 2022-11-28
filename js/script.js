dd238ddb78caa13e4cb2297d1be54f5e530e0fe9
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-28 18:21:52 +0100

    delete console.log

--- js/script.js
+++ js/script.js
@@ -12,17 +12,14 @@ button_get.addEventListener('click', function(){
     console.log(input_age)
     if(input_age == 'Maggiorenne'){
         let cost=(input_tag * 0.21).toFixed(2) + euro;
-        console.log(cost)
         document.getElementById('costo').innerHTML=cost
     }
     else if(input_age == 'Over-65'){
         let cost_65=(input_tag * 0.21 * 0.6).toFixed(2) + euro;
-        console.log(cost_65)
         document.getElementById('costo').innerHTML=cost_65
     }
     else{
         let cost_under_18=(input_tag * 0.21 * 0.8).toFixed(2) + euro;
-        console.log(cost_under_18)
         document.getElementById('costo').innerHTML=cost_under_18
     }
 })
