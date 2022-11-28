4d6313ca8fdcc4093dd837cac3ed3b3712121c92
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-28 16:27:05 +0100

    milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 const button_get=document.getElementById("button-genera");
 
+const euro= '€';
+
 button_get.addEventListener('click', function(){
     let input_tag=document.getElementById('km').value;
     console.log(input_tag)
-    let input_sex=document.getElementById('sex').value;
-    console.log(input_sex)
+    let input_age=document.getElementById('age').value;
+    console.log(input_age)
+    if(input_age == 'Maggiorenne'){
+        let cost=(input_tag * 0.21).toFixed(2) + euro;
+        console.log(cost)
+    }
+    else if(input_age == 'Over-65'){
+        let cost_65=(input_tag * 0.21 * 0.6).toFixed(2) + euro;
+        console.log(cost_65)
+    }
+    else{
+        let cost_under_18=(input_tag * 0.21 * 0.8).toFixed(2) + euro;
+        console.log(cost_under_18)
+    }
 })
 
-if(input_sex == 'Maggiorenne'){
-    let cost=input_tag * 0.21
-    console.log(cost)
 
-    
-}
 
