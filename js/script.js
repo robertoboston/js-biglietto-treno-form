e622e50a2702cb32f329dfa71bccdd650342ee9a
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-29 09:56:53 +0100

    delete console.log

--- js/script.js
+++ js/script.js
@@ -4,22 +4,19 @@ const euro= '€';
 
 button_get.addEventListener('click', function(){
     let input_name=document.getElementById('nome-cognome').value;
-    console.log(input_name)
     document.getElementById('name').innerHTML=input_name;
     let input_tag=document.getElementById('km').value;
-    console.log(input_tag)
     let input_age=document.getElementById('age').value;
-    console.log(input_age)
     if(input_age == 'Maggiorenne'){
-        let cost=(input_tag * 0.21).toFixed(2) + euro;
+        let cost = (input_tag * 0.21).toFixed(2) + euro;
         document.getElementById('costo').innerHTML=cost
     }
     else if(input_age == 'Over-65'){
-        let cost_65=(input_tag * 0.21 * 0.6).toFixed(2) + euro;
+        let cost_65 = (input_tag * 0.21 * 0.6).toFixed(2) + euro;
         document.getElementById('costo').innerHTML=cost_65
     }
     else{
-        let cost_under_18=(input_tag * 0.21 * 0.8).toFixed(2) + euro;
+        let cost_under_18 = (input_tag * 0.21 * 0.8).toFixed(2) + euro;
         document.getElementById('costo').innerHTML=cost_under_18
     }
 })
